1eb5cd74db09858b8d686fd96d558a288175ad69
censhin <censhin@gmail.com>
2013-04-06 03:23:24 +0000

    overloaded methods _list, _get, and _create

--- pyrax/cloudmonitoring.py
+++ pyrax/cloudmonitoring.py
@@ -48,6 +48,60 @@ class CloudMonitoringCheckType(BaseResource):
     pass
 
 
+class CloudMonitoringManager(BaseManager):
+    """
+    Created to override the methods _list, _get, and
+    _create to change body[self.response_key] to body.
+    """
+    def _list(self, uri, obj_class=None, body=None):
+        """
+        Handles the communication with the API when gettings
+        a full listing of the resources managed by this class.
+        """
+        if body:
+            _resp, body = self.api.method_post(uri, body=body)
+        else:
+            _resp, body = self.api.method_get(uri)
+
+        if obj_class is None:
+            obj_class = self.resource_class
+
+        data = body
+        # Note(ja): keystone returns values as list as {"values": [ ... ]}
+        #           unlike other services which just return the list...
+        if isinstance(data, dict):
+            try:
+                data = data["values"]
+            except KeyError:
+                pass
+        return [obj_class(self, res, loaded=False)
+                for res in data if res]
+
+
+    def _get(self, uri):
+        """
+        Handles the communication with the API when getting
+        a specific resource managed by this class.
+        """
+        _resp, body = self.api.method_get(uri)
+        return self.resource_class(self, body, loaded=True)
+
+
+    def _create(self, uri, body, return_none=False, return_raw=False, **kwargs):
+        """
+        Handles the communication with the API when creating a new
+        resource managed by this class.
+        """
+        self.run_hooks("modify_body_for_create", body, **kwargs)
+        _resp, body = self.api.method_post(uri, body=body)
+        if return_none:
+            # No response body
+            return
+        if return_raw:
+            return body
+        return self.resource_class(self, body)
+
+
 class CloudMonitoringClient(BaseClient):
     """
     This is the primary class for interacting with Cloud Monitoring.
@@ -56,7 +110,8 @@ class CloudMonitoringClient(BaseClient):
         """
         Create the manager to handle instances.
         """
-        self._manager = BaseManager(self)
+        self._manager = BaseManager(self,
+                resource_class=CloudMonitoringEntity, uri_base="entities")
         self._check_manager = BaseManager(self)
         self._check_type_manager = BaseManager(self)
 
